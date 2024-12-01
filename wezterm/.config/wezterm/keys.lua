d5fe3fbd3b855b39e91bf73d4e54a9445bf880dd
chuchitrii <chukhinwald@gmail.com>
2024-12-01 18:30:07 +0400

    Add zsh config. Add smart-splits.nvim

--- wezterm/.config/wezterm/keys.lua
+++ wezterm/.config/wezterm/keys.lua
@@ -1,6 +1,40 @@
 local wezterm = require("wezterm")
 local act = wezterm.action
 
+local function is_vim(pane)
+	return pane:get_user_vars().IS_NVIM == "true"
+end
+local direction_keys = {
+	Left = "h",
+	Down = "j",
+	Up = "k",
+	Right = "l",
+	h = "Left",
+	j = "Down",
+	k = "Up",
+	l = "Right",
+}
+
+local function split_nav(resize_or_move, key)
+	return {
+		key = key,
+		mods = resize_or_move == "resize" and "META" or "CTRL",
+		action = wezterm.action_callback(function(win, pane)
+			if is_vim(pane) then
+				win:perform_action({
+					SendKey = { key = key, mods = resize_or_move == "resize" and "META" or "CTRL" },
+				}, pane)
+			else
+				if resize_or_move == "resize" then
+					win:perform_action({ AdjustPaneSize = { direction_keys[key], 3 } }, pane)
+				else
+					win:perform_action({ ActivatePaneDirection = direction_keys[key] }, pane)
+				end
+			end
+		end),
+	}
+end
+
 local function keys(config)
 	config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 }
 	config.keys = {
@@ -58,6 +92,14 @@ local function keys(config)
 			mods = "LEADER",
 			action = act.CloseCurrentPane({ confirm = true }),
 		},
+		split_nav("move", "h"),
+		split_nav("move", "j"),
+		split_nav("move", "k"),
+		split_nav("move", "l"),
+		split_nav("resize", "h"),
+		split_nav("resize", "j"),
+		split_nav("resize", "k"),
+		split_nav("resize", "l"),
 	}
 	for i = 1, 9 do
 		table.insert(config.keys, {
